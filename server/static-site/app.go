c96b8959b9c87c6e490d5d2f02c12b4677d51ae3
okamuuu <okamuuu@gmail.com>
2015-08-06 18:49:26 +0900

    add static site

---
+++ server/static-site/app.go
@@ -0,0 +1,30 @@
+package main
+
+/*
+	http://www.alexedwards.net/blog/serving-static-sites-with-go
+*/
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", serveTemplate)
+
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+	lp := path.Join("templates", "layout.html")
+	fp := path.Join("templates", r.URL.Path)
+
+	tmpl, _ := template.ParseFiles(lp, fp)
+	tmpl.ExecuteTemplate(w, "layout", nil)
+}
